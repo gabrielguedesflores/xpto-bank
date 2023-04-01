21e09175a5d862e456c7e7c6b5ae5924bc336878
gabrielguedesflores <gabrielguedesflores@gmail.com>
2023-04-01 05:03:12 -0300

    subindo projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Home from './components/Home';
+import { AppState } from './dto/App..dto';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      step: 0
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ step: 1 });
+  }
+
+  render() {
+    const { step } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          {step === 0 && <Home />}
+          {step === 1 && <div>Etapa 1 do formulário</div>}
+          {step === 2 && <div>Etapa 2 do formulário</div>}
+          {/* adicione mais etapas aqui */}
+          <button 
+            id="create-account" 
+            className="btn btn-lg btn-secondary fw-bold border-white bg-dark"
+            onClick={this.handleClick}
+          >
+            Criar uma conta
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const CoverExample: React.FC = () => {
+  return (
+    <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+      <main className="px-3">
+        <h1>Olá, tudo bem? Somos a XPTOBank</h1>
+        <p className="lead">
+          Para criar sua conta, siga o passo a passo abaixo
+        </p>
+        <br />
+        <p className="lead">
+          
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default CoverExample;
